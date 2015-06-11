4a019f43959944cddb37c524ec4084f8ea1bb033
Adam Christensen <adamchristensen@ymail.com>
2015-06-11 11:15:16 -0400

    brainfuck lab

--- src/brainfuck.cpp
+++ src/brainfuck.cpp
@@ -106,17 +106,24 @@ class Program : public Container {
  * Modify as necessary and add whatever functions you need to get things done.
  */
 void parse(fstream & file, Container * container) {
-    char c;
-    // How to peek at the next character
-    c = (char)file.peek();
-    // How to print out that character
-    cout << c;
-    // How to read a character from the file and advance to the next character
-    file >> c;
-    // How to print out that character
-    cout << c;
-    // How to insert a node into the container.
-    container->children.push_back(new CommandNode(c));
+Loop * l;
+	char c;
+	while(file >> c) {
+		switch (c) {
+			case '[':
+				l = new Loop();
+				container->children.push_back(l);
+				parse(file, l);
+				break;
+			case ']':
+				return;
+			case '+': case '-': case '.': case ',': case '>': case '<':
+				container->children.push_back(new CommandNode(c));
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 /**
